0ab29d1a04fb2e7f1414ac22ef838480eba391bd
Ragnar Stefánsson <ragnarstefanss@gmail.com>
2022-11-21 15:21:54 +0000

    Header change layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,14 @@
-import {BsSun, BsFillMoonFill} from "react-icons/bs"
+import { BsSun, BsFillMoonFill } from "react-icons/bs";
+import { toggleTheme } from "../../features/theme/themeSlice";
+import { useAppDispatch, useAppSelector } from "../../hooks/storeHook";
 
 const Header = () => {
+    const { darkTheme } = useAppSelector((state) => state);
+
+    const dispatch = useAppDispatch();
+
+    const onToggle = () => dispatch(toggleTheme());
+
     return (
         <header className="mb-20">
             <nav className="border-b border-gray-200 border-opacity-25 py-2.5">
@@ -10,10 +18,20 @@ const Header = () => {
                             Movies
                         </span>
                     </a>
-                
+
                     <div className="flex items-center lg:order-2">
-                        <BsSun className="hover:opacity-50 cursor-pointer"/>
-                        <BsFillMoonFill className="hover:opacity-50 cursor-pointer"/>
+                        {darkTheme && (
+                            <BsSun
+                                onClick={() => onToggle()}
+                                className="hover:opacity-50 cursor-pointer"
+                            />
+                        )}
+                        {!darkTheme && (
+                            <BsFillMoonFill
+                                onClick={() => onToggle()}
+                                className="hover:opacity-50 cursor-pointer"
+                            />
+                        )}
                     </div>
                 </div>
             </nav>
@@ -21,22 +39,11 @@ const Header = () => {
     );
 };
 
-
 export default Header;
 
-//                             
-// import { BsSun, BsFillMoonFill } from "react-icons/bs";
-
-// import { toggleTheme } from "../../features/theme/themeSlice";
-// import { useAppDispatch, useAppSelector } from "../../hooks/storeHook";
+// import {BsSun, BsFillMoonFill} from "react-icons/bs"
 
 // const Header = () => {
-//     const { darkTheme } = useAppSelector((state) => state);
-
-//     const dispatch = useAppDispatch();
-
-//     const onToggle = () => dispatch(toggleTheme());
-
 //     return (
 //         <header className="mb-20">
 //             <nav className="border-b border-gray-200 border-opacity-25 py-2.5">
@@ -46,25 +53,16 @@ export default Header;
 //                             Movies
 //                         </span>
 //                     </a>
-
+                
 //                     <div className="flex items-center lg:order-2">
-//                         {darkTheme && (
-//                             <BsSun
-//                                 onClick={() => onToggle()}
-//                                 className="hover:opacity-50 cursor-pointer"
-//                             />
-//                         )}
-//                         {!darkTheme && (
-//                             <BsFillMoonFill
-//                                 onClick={() => onToggle()}
-//                                 className="hover:opacity-50 cursor-pointer"
-//                             />
-//                         )}
+//                         <BsSun className="hover:opacity-50 cursor-pointer"/>
+//                         <BsFillMoonFill className="hover:opacity-50 cursor-pointer"/>
 //                     </div>
 //                 </div>
 //             </nav>
 //         </header>
 //     );
 // };
+
 
 // export default Header;
