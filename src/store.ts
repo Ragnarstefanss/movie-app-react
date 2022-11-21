1cac59c5fd9846a92f320422ae2e9abe63609209
Ragnar Stefánsson <ragnarstefanss@gmail.com>
2022-11-21 16:17:40 +0000

    Loaded data from moviedb and displayed on website

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import themeReducer from "./features/theme/themeSlice"
+import movieReducer from "./features/movies/movieSlice";
 
 export const store = configureStore({
     reducer: {
         darkTheme: themeReducer,
+        movies: movieReducer
     },
 });
 
