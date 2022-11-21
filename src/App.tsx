fd376a9e09703dd609b1a659b5e51b48bba36c55
Ragnar Stefánsson <ragnarstefanss@gmail.com>
2022-11-21 15:38:43 +0000

    theme color change

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css';
 import Header from './components/Header/Header';
+import { useAppSelector } from "./hooks/storeHook";
 
 function App() {
-  return <div className='text-red-900'>
-    <Header />
-  </div>;
+  const { darkTheme } = useAppSelector((state) => state);
+  return (
+    <div className={darkTheme ? "dark" : ""}>
+      <div className="dark:bg-red-900 dark:text-white min-h-screen px-4 lg:px-12 pb-20">
+        <Header />
+      </div>
+    </div>
+  );
 }
-
 export default App;
